d4c0177f52b7dd3d8f28c41bb91c2efe9c27203d
Turadg Aleahmad <turadg@agoric.com>
2022-06-13 14:53:31 -0700

    refactor(feeCollector): rekey RUN to Fee

--- packages/inter-protocol/test/vaultFactory/test-vaultFactory.js
+++ packages/inter-protocol/test/vaultFactory/test-vaultFactory.js
@@ -1687,7 +1687,7 @@ test('overdeposit', async t => {
   await E(collectFeesSeat).getOfferResult();
   assertAmountsEqual(
     t,
-    await E.get(E(collectFeesSeat).getCurrentAllocation()).RUN,
+    await E.get(E(collectFeesSeat).getCurrentAllocation()).Fee,
     run.make(300n),
   );
 });
@@ -2017,7 +2017,7 @@ test('collect fees from loan and AMM', async t => {
   );
   await E(collectFeesSeat).getOfferResult();
   const feePayoutAmount = await E.get(E(collectFeesSeat).getCurrentAllocation())
-    .RUN;
+    .Fee;
   trace(t, 'Fee', feePoolBalance, feePayoutAmount);
   t.truthy(AmountMath.isGTE(feePayoutAmount, feePoolBalance.RUN));
 });
